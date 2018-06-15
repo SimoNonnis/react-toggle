684ded9d2e60efc395002aad9b25536a23cd2531
Simon Nonnis <simon.nonnis@protonmail.com>
2018-06-15 10:29:07 +0100

    :art: Add onClick()

--- src/components/Toggle/index.jsx
+++ src/components/Toggle/index.jsx
@@ -4,11 +4,20 @@ import Switch from '../Switch';
 import { Container, Title } from './style';
 
 class Toggle extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      on: false
+    };
+  }
+
+  handleSwitch = () => this.setState({ on: !this.state.on });
+
   render() {
     return (
       <Container>
         <Title>Toggle</Title>
-        <Switch on />
+        <Switch on={this.state.on} onClick={this.handleSwitch} />
       </Container>
     );
   }
