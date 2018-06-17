7ee448bb1bca10661297dfc20c8388daafa50d5f
Simon Nonnis <simon.nonnis@protonmail.com>
2018-06-17 10:19:14 +0100

    :art: Make renderUI a render prop

--- src/components/Toggle/index.jsx
+++ src/components/Toggle/index.jsx
@@ -3,16 +3,6 @@ import Switch from '../Switch';
 
 import { Container, Title, Text } from './style';
 
-const renderUI = ({ on, handleSwitch }) => (
-  <Container>
-    <Title>Render Props</Title>
-    <Switch on={on} onClick={handleSwitch} />
-
-    <Text>Toggle is {on ? 'ON' : 'OFF'}</Text>
-    <button onClick={handleSwitch}>{on ? 'ON' : 'OFF'}</button>
-  </Container>
-);
-
 class Toggle extends Component {
   state = {
     on: false
@@ -21,11 +11,25 @@ class Toggle extends Component {
   handleSwitch = () => this.setState(({ on }) => ({ on: !on }));
 
   render() {
-    const { on } = this.state;
-    return renderUI({ on: on, handleSwitch: this.handleSwitch });
+    return this.props.renderUI({
+      on: this.state.on,
+      handleSwitch: this.handleSwitch
+    });
   }
 }
 
-const RenderProps = () => <Toggle />;
+const RenderProps = () => (
+  <Toggle
+    renderUI={({ on, handleSwitch }) => (
+      <Container>
+        <Title>Render Props</Title>
+        <Switch on={on} onClick={handleSwitch} />
+
+        <Text>Toggle is {on ? 'ON' : 'OFF'}</Text>
+        <button onClick={handleSwitch}>{on ? 'ON' : 'OFF'}</button>
+      </Container>
+    )}
+  />
+);
 
 export default RenderProps;
